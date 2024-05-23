28d6b17808b9af5b63371241ffc6ffd652f52cdc
WingIdeeLab <94964914+WingIdeeLab@users.noreply.github.com>
2024-05-23 13:49:56 +0200

    LIchtbox V1, alles Funktioniert

--- main.ts
+++ main.ts
@@ -1,38 +1,71 @@
 let Potiwert = 0
 let SensorWert = 0
 basic.forever(function () {
-    Potiwert = pins.analogReadPin(AnalogPin.P1) / 204
-    SensorWert = pins.analogReadPin(AnalogPin.P2) / 204
-    serial.writeLine("" + (Potiwert))
-    serial.writeLine("" + (SensorWert))
-    serial.writeLine("" + (pins.digitalReadPin(DigitalPin.P0)))
-    if (SensorWert > Potiwert) {
-        basic.setLedColor(0x00ff00)
-    } else {
-        basic.setLedColor(0xff0000)
-    }
-    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        for (let Index = 0; Index <= 4; Index++) {
-            led.plot(4, Index)
+    Potiwert = pins.analogReadPin(AnalogPin.C16) / 250
+    SensorWert = pins.analogReadPin(AnalogPin.P2) / 200
+    // Wenn beide Sensoren eingesteckt --> Grün
+    // Falls nur LIcht eingesteckt --> Gelb
+    // Falls nur PIR eingesteckt --> Blau
+    if (input.pinIsPressed(TouchPin.P1) && input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        basic.setLedColor(basic.rgb(0, 25, 0))
+        led.plot(1, Potiwert)
+        led.plot(2, Potiwert)
+        for (let Index = 0; Index <= SensorWert; Index++) {
+            led.plot(0, Index)
+        }
+        // Falls Licht unter Grnezwert
+        // Falls PIR Aktiviert
+        // Falls Nur PIR Aktiviert
+        if (SensorWert < Potiwert && !(input.pinIsPressed(TouchPin.P3))) {
+            basic.setLedColor(basic.rgb(0, 255, 0))
+            for (let Index = 0; Index <= 4; Index++) {
+                led.plot(4, Index)
+            }
+        } else if (!(input.pinIsPressed(TouchPin.P3))) {
+            for (let Index = 0; Index <= 4; Index++) {
+                led.plot(4, Index)
+            }
+        } else {
+        	
+        }
+    } else if (input.pinIsPressed(TouchPin.P1) && !(input.buttonIsPressed(Button.B))) {
+        // Falls LIcht unter den Grenzwert
+        if (SensorWert < Potiwert) {
+            basic.clearScreen()
+            basic.setLedColor(basic.rgb(255, 255, 0))
+            led.plot(1, Potiwert)
+            led.plot(2, Potiwert)
+            for (let Index = 0; Index <= SensorWert; Index++) {
+                led.plot(0, Index)
+            }
+        } else {
+            basic.clearScreen()
+            basic.setLedColor(basic.rgb(25, 25, 0))
+            led.plot(1, Potiwert)
+            led.plot(2, Potiwert)
+            for (let Index = 0; Index <= SensorWert; Index++) {
+                led.plot(0, Index)
+            }
+        }
+    } else if (!(input.pinIsPressed(TouchPin.P1)) && input.buttonIsPressed(Button.B)) {
+        // Falls PIR Aktiviert
+        if (!(input.pinIsPressed(TouchPin.P3))) {
+            basic.clearScreen()
+            basic.setLedColor(basic.rgb(0, 0, 255))
+            for (let Index = 0; Index <= 4; Index++) {
+                led.plot(4, Index)
+            }
+        } else {
+            basic.clearScreen()
+            basic.setLedColor(basic.rgb(0, 0, 25))
+            for (let Index = 0; Index <= 4; Index++) {
+                led.unplot(4, Index)
+            }
         }
     } else {
-        for (let Index = 0; Index <= 4; Index++) {
-            led.unplot(4, Index)
-        }
+        basic.clearScreen()
+        basic.showIcon(IconNames.Happy)
     }
-    for (let Index = 0; Index <= 4; Index++) {
-        led.unplot(0, Index)
-    }
-    for (let Index = 0; Index <= 4; Index++) {
-        led.unplot(1, Index)
-    }
-    for (let Index = 0; Index <= 4; Index++) {
-        led.unplot(2, Index)
-    }
-    for (let Index = 0; Index <= SensorWert; Index++) {
-        led.plot(0, Index)
-    }
-    led.plot(1, Potiwert)
-    led.plot(2, Potiwert)
     control.waitMicros(100000)
 })
